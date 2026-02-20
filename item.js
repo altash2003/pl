d64fcb2835d39a024f967a8324e8896c18468edb
altash2003 <altash2003@gmail.com>
2026-02-20 13:16:37 +0800

    Delete item.js

--- item.js
+++
@@ -1,7 +0,0 @@
-// Put icon files in /icons and reference like: "./icons/pacman-pack.png"
-window.ITEMS = [
-  { icon: "./icons/pacman-pack.png", name: "Pacman Pack", price: "300,000-400,000" },
-  { icon: "./icons/pacman-hair.png", name: "Pacman Hair", price: "60,000-100,000" },
-  { icon: "./icons/pacman-eyes.png", name: "Pacman Eyes", price: "60,000-100,000" },
-  { icon: "./icons/pacman-cloak.png", name: "Pacman Cloak", price: "60,000-100,000" },
-];
